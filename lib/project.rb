89a6a0c63e2c2eb4afe01d9007f0811eea1ad8ff
Tanya Hodges <tanyac52003@gmail.com>
2020-06-10 14:38:15 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,7 +8,7 @@ class Project
   
   def add_backer(backer)
     @backers << backer
-    backer.back_project(self) unless backer.backed_projects.include?(self)
+    backer.backed_projects
   end 
   
   
